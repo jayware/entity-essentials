6e3eadec271aaa8d18093b788d4052f70feeb903
Elmar Schug <elmar.schug@jayware.org>
2016-05-22 09:21:07 +0200

    Added JMockit to the test dependencies and implemented to tests for SubscriptionImpl_StrongReference.java and SubscriptionImpl_WeakReference.java

---
+++ entity-essentials-impl/src/test/java/org/jayware/e2/event/impl/SubscriptionImplTest_WeakReference.java
@@ -0,0 +1,51 @@
+package org.jayware.e2.event.impl;
+
+import mockit.Mocked;
+import org.jayware.e2.event.api.EventDispatcher;
+import org.jayware.e2.event.api.EventFilter;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+public class SubscriptionImplTest_WeakReference
+{
+    private SubscriptionImpl_WeakReference testee;
+
+    private @Mocked Object testSubscriber;
+    private @Mocked EventDispatcher testDispatcher;
+    private @Mocked EventFilter testFilterA;
+    private @Mocked EventFilter testFilterB;
+
+    @BeforeMethod
+    public void setUp()
+    throws Exception
+    {
+        testee = new SubscriptionImpl_WeakReference(testSubscriber, testDispatcher, new EventFilter[] {testFilterA, testFilterB});
+    }
+
+    @Test
+    public void test()
+    throws Exception
+    {
+        assertThat(testee.getSubscriber()).isEqualTo(testSubscriber);
+        assertThat(testee.getEventDispatcher()).isEqualTo(testDispatcher);
+        assertThat(testee.getFilters()).containsExactlyInAnyOrder(testFilterA, testFilterB);
+    }
+
+    @Test
+    public void test_isValid_Returns_true_for_a_newly_constructed_subscription()
+    throws Exception
+    {
+        assertThat(testee.isValid()).isTrue();
+    }
+
+    @Test
+    public void test_isValid_Returns_false_when_subscription_gets_invalidated()
+    throws Exception
+    {
+        testee.invalidate();
+        assertThat(testee.isValid()).isFalse();
+    }
+}
