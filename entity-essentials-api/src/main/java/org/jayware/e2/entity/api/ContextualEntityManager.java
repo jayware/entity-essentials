6bb79f3b1a766ed6279f947f83c35f9e2e5a90ed
Elmar Schug <elmar.schug@jayware.org>
2016-07-07 13:58:13 +0200

    Wrote javadoc for the ContextualEntityManager.java

--- entity-essentials-api/src/main/java/org/jayware/e2/entity/api/ContextualEntityManager.java
+++ entity-essentials-api/src/main/java/org/jayware/e2/entity/api/ContextualEntityManager.java
@@ -22,6 +22,7 @@
 package org.jayware.e2.entity.api;
 
 import org.jayware.e2.component.api.Aspect;
+import org.jayware.e2.context.api.Context;
 import org.jayware.e2.context.api.Contextual;
 import org.jayware.e2.context.api.IllegalContextException;
 import org.jayware.e2.util.Filter;
@@ -29,18 +30,83 @@ import org.jayware.e2.util.Filter;
 import java.util.List;
 
 
+/**
+ *
+ */
 public interface ContextualEntityManager
 extends Contextual
 {
+    /**
+     * Creates an {@link Entity} within the {@link Context} of this {@link ContextualEntityManager}.
+     *
+     * @return an {@link EntityRef} of the newly created {@link Entity}.
+     *
+     * @throws IllegalStateException if the {@link Context} of this {@link ContextualEntityManager} has been disposed.
+     */
     EntityRef createEntity() throws IllegalStateException;
 
+    /**
+     * Deletes the {@link Entity} denoted by the specified {@link EntityRef} within the {@link Context} of this {@link ContextualEntityManager}.
+     *
+     * @param ref an {@link EntityRef}.
+     *
+     * @throws IllegalArgumentException if the specified {@link EntityRef} is <code>null</code>.
+     *
+     * @throws IllegalStateException if the {@link Context} of this {@link ContextualEntityManager} has been disposed.
+     *
+     * @throws IllegalContextException if the specified {@link EntityRef} belongs to another {@link Context}.
+     */
     void deleteEntity(EntityRef ref) throws IllegalArgumentException, IllegalStateException, IllegalContextException;
 
+    /**
+     * Returns a {@link List} of all {@link Entity Entities} within the {@link Context} of this {@link ContextualEntityManager}.
+     *
+     * @return a {@link List} of {@link EntityRef}s in any order, never <code>null</code>.
+     *
+     * @throws IllegalStateException if the {@link Context} of this {@link ContextualEntityManager} has been disposed.
+     */
     List<EntityRef> findEntities() throws IllegalStateException;
 
+    /**
+     * Returns a {@link List} of {@link EntityRef}s for all {@link Entity Entities} within the {@link Context} of this
+     * {@link ContextualEntityManager} which match the specified {@link Aspect}.
+     *
+     * @param aspect an {@link Aspect}.
+     *
+     * @return a {@link List} of {@link EntityRef}s in any order, never <code>null</code>.
+     *
+     * @throws IllegalArgumentException if the specified {@link Aspect} is <code>null</code>.
+     *
+     * @throws IllegalStateException if the {@link Context} of this {@link ContextualEntityManager} has been disposed.
+     */
     List<EntityRef> findEntities(Aspect aspect) throws IllegalArgumentException, IllegalStateException;
 
+    /**
+     * Returns a {@link List} of {@link EntityRef}s for all {@link Entity Entities} within the {@link Context} of this
+     * {@link ContextualEntityManager} which pass specified {@link Filter}s.
+     *
+     * @param filters one or more {@link Filter}s.
+     *
+     * @return a {@link List} of {@link EntityRef}s in any order, never <code>null</code>.
+     *
+     * @throws IllegalArgumentException if the specified {@link Aspect} is <code>null</code>.
+     *
+     * @throws IllegalStateException if the {@link Context} of this {@link ContextualEntityManager} has been disposed.
+     */
     List<EntityRef> findEntities(Filter<EntityRef>... filters) throws IllegalArgumentException, IllegalStateException;
 
+    /**
+     * Returns a {@link List} of {@link EntityRef}s for all {@link Entity Entities} within the {@link Context} of this {@link ContextualEntityManager}
+     * which match the specified {@link Aspect} and pass specified {@link Filter}s.
+     *
+     * @param aspect an {@link Aspect}.
+     * @param filters one or more {@link Filter}s.
+     *
+     * @return a {@link List} of {@link EntityRef}s in any order, never <code>null</code>.
+     *
+     * @throws IllegalArgumentException if the specified {@link Aspect} is <code>null</code> or <code>null</code> is passed as vararg.
+     *
+     * @throws IllegalStateException if the {@link Context} of this {@link ContextualEntityManager} has been disposed.
+     */
     List<EntityRef> findEntities(Aspect aspect, Filter<EntityRef>... filters) throws IllegalArgumentException, IllegalStateException;
 }
