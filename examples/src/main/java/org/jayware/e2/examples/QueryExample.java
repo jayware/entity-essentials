d228ad7dc17df6da21756b509263ed224998012e
Elmar Schug <elmar.schug@jayware.org>
2017-01-14 15:05:03 +0100

    Changed license of entity essentials to apache license 2.0.

--- examples/src/main/java/org/jayware/e2/examples/QueryExample.java
+++ examples/src/main/java/org/jayware/e2/examples/QueryExample.java
@@ -1,23 +1,20 @@
 /**
  * Entity Essentials -- A Component-based Entity System
  *
- * Copyright (C) 2016 Elmar Schug <elmar.schug@jayware.org>,
+ * Copyright (C) 2017 Elmar Schug <elmar.schug@jayware.org>,
  *                    Markus Neubauer <markus.neubauer@jayware.org>
  *
- *     This file is part of Entity Essentials.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     Entity Essentials is free software; you can redistribute it and/or
- *     modify it under the terms of the GNU Lesser General Public License
- *     as published by the Free Software Foundation, either version 3 of
- *     the License, or any later version.
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *     Entity Essentials is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- *     Lesser General Public License for more details.
- *
- *     You should have received a copy of the GNU Lesser General Public License
- *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jayware.e2.examples;
 
